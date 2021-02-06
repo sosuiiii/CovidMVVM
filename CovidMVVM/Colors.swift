dd36d68eed0f75b3dc077c6e52ba2c37453735f4
sosuiiii <wntsssbbsnws@yahoo.co.jp>
2021-02-06 18:32:33 +0900

    トップ画面作成完了

--- CovidMVVM/Colors.swift
+++ CovidMVVM/Colors.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+import UIKit
+
+struct Colors {
+    static let bluePurple = UIColor(red: 87/255, green: 99/255, blue: 175/255, alpha: 1)
+    static let blue = UIColor(red: 92/255, green: 137/255, blue: 200/255, alpha: 1)
+    static let blueGreen = UIColor(red: 86/255, green: 196/255, blue: 197/255, alpha: 1)
+    static let green = UIColor(red: 100/255, green: 194/255, blue: 150/255, alpha: 1)
+    static let yellowGreen = UIColor(red: 173/255, green: 215/255, blue: 134/255, alpha: 1)
+    static let yellow = UIColor(red: 251/255, green: 245/255, blue: 131/255, alpha: 1)
+    static let yellowOrange = UIColor(red: 252/255, green: 200/255, blue: 120/255, alpha: 1)
+    static let orange = UIColor(red: 246/255, green: 169/255, blue: 115/255, alpha: 1)
+    static let redOrange = UIColor(red: 245/255, green: 141/255, blue: 116/255, alpha: 1)
+    static let red = UIColor(red: 245/255, green: 121/255, blue: 109/255, alpha: 1)
+    static let redPurple = UIColor(red: 190/255, green: 125/255, blue: 181/255, alpha: 1)
+    static let purple = UIColor(red: 129/255, green: 107/255, blue: 177/255, alpha: 1)
+    static let white = UIColor.systemGray6
+    static let black = UIColor(red: 0, green: 0, blue: 0, alpha: 0.9)
+}
--- CovidMVVM/View/TopViewController.swift
+++ CovidMVVM/View/TopViewController.swift
@@ -15,7 +15,6 @@ import PKHUD
 class TopViewController: UIViewController, StoryboardInstantiatable {
     
     //MARK: DI
-    var viewModel: TopViewModelType!
     struct Dependency {
         let viewModel: TopViewModelType!
     }
@@ -23,13 +22,36 @@ class TopViewController: UIViewController, StoryboardInstantiatable {
         viewModel = dependency.viewModel
     }
     
+    //MARK: Properties
+    var viewModel: TopViewModelType!
     let disposeBag = DisposeBag()
+    
+    let centerView = UIView()
+    let pcr = UILabel() //PCR
+    let pcrNum = UILabel()
+    let positive = UILabel() //感染
+    let positiveNum = UILabel()
+    let hospitalize = UILabel()  //入院
+    let hospitalizeNum = UILabel()
+    let severe = UILabel() //重症
+    let severeNum = UILabel()
+    let death = UILabel() //死者
+    let deathNum = UILabel()
+    let discharge = UILabel() //退院
+    let dischargeNum = UILabel()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        self.navigationController?.navigationBar.isHidden = true
         
+        //MARK: ビューのセットアップ
+        self.navigationController?.navigationBar.isHidden = true
+        view.backgroundColor = .systemGray6
+        setupGradation()
+        setupCenterView()
+        setupContent()
+        setupAPILabel()
+        setupAPILabel()
         
         //MARK: Inputs
         let _ = viewModel.inputs.fetchCovidTotal.onNext(Void())
@@ -57,9 +79,148 @@ class TopViewController: UIViewController, StoryboardInstantiatable {
             .subscribe(onNext: { [weak self] element in
                 guard let self = self else {return}
                 guard let element = element else {return}
+                DispatchQueue.main.async {
+                    self.pcrNum.text = "\(element.pcr)"
+                    self.positiveNum.text = "\(element.positive)"
+                    self.hospitalizeNum.text = "\(element.hospitalize)"
+                    self.severeNum.text = "\(element.severe)"
+                    self.deathNum.text = "\(element.death)"
+                    self.dischargeNum.text = "\(element.discharge)"
+                }
             }).disposed(by: disposeBag)
     }
-
-
+    
+    @objc func reloadAction() {
+        loadView()
+        viewDidLoad()
+    }
+    @objc func chatAction() {
+        performSegue(withIdentifier: "goChat", sender: nil)
+    }
+    @objc func goHealthCheck() {
+        performSegue(withIdentifier: "goHealthCheck", sender: nil)
+    }
+    @objc func goChart() {
+        performSegue(withIdentifier: "goChart", sender: nil)
+    }
 }
 
+//MARK: ビュー
+extension TopViewController {
+    //背景トップのグラデーション
+    func setupGradation() {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = CGRect(x: 0, y: 0, width: view.frame.size.width, height: view.frame.size.height / 2)
+        gradientLayer.colors = [Colors.bluePurple.cgColor, Colors.blue.cgColor]
+        gradientLayer.startPoint = CGPoint(x: 0, y: 0)
+        gradientLayer.endPoint = CGPoint(x: 1, y: 1)
+        view.layer.insertSublayer(gradientLayer, at:0)
+    }
+    //真ん中のビュー
+    func setupCenterView() {
+        centerView.frame.size = CGSize(width: view.frame.size.width, height: 340)
+        centerView.center = CGPoint(x: view.center.x, y: view.center.y)
+        centerView.backgroundColor = .white
+        centerView.layer.cornerRadius = 30
+        centerView.layer.shadowOffset = CGSize(width: 2, height: 2)
+        centerView.layer.shadowColor = UIColor.gray.cgColor
+        centerView.layer.shadowOpacity = 0.5
+        view.addSubview(centerView)
+    }
+    //真ん中のビューの数値
+    func setupAPILabel() {
+        let size = CGSize(width: 200, height: 40)
+        let leftX = view.frame.size.width * 0.38
+        let rightX = view.frame.size.width * 0.85
+        let font = UIFont.systemFont(ofSize: 35, weight: .heavy)
+        let color = Colors.blue
+        createAPILabel(pcrNum, size: size, centerX: leftX, y: 60, font: font, color: color)
+        createAPILabel(positiveNum, size: size, centerX: rightX, y: 60, font: font, color: color)
+        createAPILabel(hospitalizeNum, size: size, centerX: leftX, y: 160, font: font, color: color)
+        createAPILabel(severeNum, size: size, centerX: rightX, y: 160, font: font, color: color)
+        createAPILabel(deathNum, size: size, centerX: leftX, y: 260, font: font, color: color)
+        createAPILabel(dischargeNum, size: size, centerX: rightX, y: 260, font: font, color: color)
+        
+    }
+    func createAPILabel(_ label: UILabel, size: CGSize, centerX: CGFloat,y: CGFloat, font: UIFont, color: UIColor) {
+        label.frame.size = size
+        label.center.x = centerX
+        label.frame.origin.y = y
+        label.font = font
+        label.textColor = color
+        centerView.addSubview(label)
+    }
+    
+    //タイトル、APIデータのタイトル、下部ボタン
+    func setupContent() {
+         
+        let labelFont = UIFont.systemFont(ofSize: 15, weight: .heavy)
+        let size = CGSize(width: 150, height: 50)
+        let color = Colors.bluePurple
+        let leftX = view.frame.size.width * 0.33
+        let rightX = view.frame.size.width * 0.80
+        createLabelTitle("Covid in Japan",
+                   size: CGSize(width: 180, height: 35),
+                   centerX: view.center.x - 20, y: -60,
+                   font: .systemFont(ofSize: 25, weight: .heavy),
+                   color: .white)
+        createLabelTitle("PCR数", size: size, centerX:  leftX, y: 20, font: labelFont, color: color)
+        createLabelTitle("感染者数", size: size, centerX: rightX , y: 20, font: labelFont, color: color)
+        createLabelTitle("入院者数", size: size, centerX: leftX , y: 120, font: labelFont, color: color)
+        createLabelTitle("重傷者数", size: size, centerX: rightX , y: 120, font: labelFont, color: color)
+        createLabelTitle("死者数", size: size, centerX: leftX , y: 220, font: labelFont, color: color)
+        createLabelTitle("退院者数", size: size, centerX: rightX , y: 220, font: labelFont, color: color)
+        
+        let height = view.frame.size.height / 2
+        createButton("健康管理", size: size, y: height + 190, color: Colors.blue, parentView: view).addTarget(self, action: #selector(goHealthCheck), for: .touchDown)
+        createButton("県別状況", size: size, y: height + 240, color: Colors.blue, parentView: view)
+            .addTarget(self, action: #selector(goChart), for: .touchDown)
+        
+        createImageButton("chat", x: view.frame.size.width - 50).addTarget(self, action: #selector(chatAction), for: .touchDown)
+        createImageButton("reload", x: 10).addTarget(self, action: #selector(reloadAction), for: .touchDown)
+        
+        let imageView = UIImageView()
+        let image = UIImage(named: "virus")
+        imageView.image = image
+        imageView.frame = CGRect(x: view.frame.size.width, y: -65, width: 50, height: 50)
+        centerView.addSubview(imageView)
+        UIView.animate(withDuration: 1.5, delay: 0.5, options: [.curveEaseIn], animations: {
+            imageView.frame = CGRect(x: self.view.frame.size.width - 100, y: -65, width: 50, height: 50)
+            imageView.transform = CGAffineTransform(rotationAngle: -90)
+        }, completion: nil)
+    }
+    //APIデータのタイトル
+    func createLabelTitle(_ text: String, size: CGSize, centerX: CGFloat,y: CGFloat, font: UIFont, color: UIColor){
+        let label = UILabel()
+        label.text = text
+        label.frame.size = size
+        label.center.x = centerX
+        label.frame.origin.y = y
+        label.font = font
+        label.textColor = color
+        centerView.addSubview(label)
+    }
+    //下部ボタン
+    func createButton(_ title: String, size: CGSize, y: CGFloat, color: UIColor, parentView: UIView) -> UIButton{
+        let button = UIButton(type: .system)
+        button.setTitle(title, for: .normal)
+        button.frame.size = size
+        button.center.x = view.center.x
+        let attributedTitle = NSAttributedString(string: title, attributes: [NSAttributedString.Key.kern: 8.0])
+        button.setAttributedTitle(attributedTitle, for: .normal)
+        button.frame.origin.y = y
+        button.setTitleColor(color, for: .normal)
+        parentView.addSubview(button)
+        return button
+    }
+    //上部ボタン
+    func createImageButton(_ name: String, x: CGFloat) -> UIButton {
+        let button = UIButton(type: .system)
+        button.setImage(UIImage(named: name), for: .normal)
+        button.frame.size = CGSize(width: 30, height: 30)
+        button.tintColor = .white
+        button.frame.origin = CGPoint(x: x, y: 25)
+        view.addSubview(button)
+        return button
+    }
+}
