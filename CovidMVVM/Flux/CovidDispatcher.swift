4f6ecd6fbed7e92da22f775e2035502f9f24be08
sosuiiii <wntsssbbsnws@yahoo.co.jp>
2021-02-06 14:30:59 +0900

    Flux定義

--- CovidMVVM/Flux/CovidDispatcher.swift
+++ CovidMVVM/Flux/CovidDispatcher.swift
@@ -6,3 +6,11 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
+
+final class CovidDispatcher {
+    let fetchCovidTotal = PublishRelay<CovidInfo.Total>()
+    let fetchCovidPrefecture = PublishRelay<[CovidInfo.Prefecture]>()
+    let error = PublishRelay<Error>()
+}
--- CovidMVVM/Flux/CovidStore.swift
+++ CovidMVVM/Flux/CovidStore.swift
@@ -6,3 +6,42 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
+
+final class CovidStore {
+    
+    private let _covidTotalResponse = BehaviorRelay<CovidInfo.Total?>(value: nil)
+    private let _covidPrefectureResponse = BehaviorRelay<[CovidInfo.Prefecture]?>(value: nil)
+    private let _error = PublishRelay<Error>()
+    private let disposeBag = DisposeBag()
+    
+    var covidTotalResponse:
+        Observable<CovidInfo.Total?> {
+        return _covidTotalResponse.asObservable()
+    }
+    var covidTotalResponseValue: CovidInfo.Total? {
+        return _covidTotalResponse.value
+    }
+    
+    var covidPrefectureResponse: Observable<[CovidInfo.Prefecture]?> {
+        return _covidPrefectureResponse.asObservable()
+    }
+    var covidPrefectureResponseValue: [CovidInfo.Prefecture]? {
+        return _covidPrefectureResponse.value
+    }
+    var error: Observable<Error> {
+        return _error.asObservable()
+    }
+    
+    init(dispatcher: CovidDispatcher) {
+        
+        let _ = dispatcher.fetchCovidTotal
+            .bind(to: _covidTotalResponse)
+            .disposed(by: disposeBag)
+        
+        let _ = dispatcher.fetchCovidPrefecture
+            .bind(to: _covidPrefectureResponse)
+            .disposed(by: disposeBag)
+    }
+}
