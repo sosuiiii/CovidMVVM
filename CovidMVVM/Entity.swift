f4906db1c5dcfc5778304be1cc631bf50a49e9a5
sosuiiii <wntsssbbsnws@yahoo.co.jp>
2021-02-06 14:18:12 +0900

    API繋ぎ込み準備

--- CovidMVVM/Entity.swift
+++ CovidMVVM/Entity.swift
@@ -6,3 +6,40 @@
 //
 
 import Foundation
+
+struct CovidInfo: Codable {
+    
+    struct Total: Codable {
+        var pcr: Int
+        var positive: Int
+        var hospitalize: Int
+        var severe: Int
+        var death: Int
+        var discharge: Int
+        
+        enum CodingKeys: String, CodingKey {
+            case pcr
+            case positive
+            case hospitalize
+            case severe
+            case death
+            case discharge
+        }
+    }
+    
+    struct Prefecture: Codable {
+        var id: Int
+        var nameJa: String
+        var cases: Int
+        var deaths: Int
+        var pcr: Int
+        
+        enum CodingKeys: String, CodingKey {
+            case id
+            case nameJa = "name_ja"
+            case cases
+            case deaths
+            case pcr
+        }
+    }
+}
--- CovidMVVM/Repository.swift
+++ CovidMVVM/Repository.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+import RxSwift
+import Moya
+
+final class CovidRepository {
+    private static let apiProvider = MoyaProvider<CovidAPI>()
+    private static let disposeBag = DisposeBag()
+}
+extension CovidRepository {
+    
+    static func getTotal() throws -> Observable<CovidInfo.Total> {
+        return apiProvider.rx.request(.getTotal)
+            .map { response in
+                return try JSONDecoder().decode(CovidInfo.Total.self, from: response.data)
+            }.asObservable()
+    }
+    static func getPrefecture() throws -> Observable<[CovidInfo.Prefecture]> {
+        return apiProvider.rx.request(.getPrefecture)
+            .map{ response in
+                return try JSONDecoder().decode([CovidInfo.Prefecture].self, from: response.data)
+            }.asObservable()
+    }
+}
